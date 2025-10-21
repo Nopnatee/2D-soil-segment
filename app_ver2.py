979d36972552ffa6058b30ef97e5b01750f8125b
Khamic672 <Khamic.sk@gmail.com>
2025-10-22 02:05:04 +0700

    new file:   2d_unet_trainer_standalone.py 	modified:   app.py 	modified:   app_ver2.py 	modified:   script.py

--- app_ver2.py
+++ app_ver2.py
@@ -31,7 +31,7 @@ class NPKPredictorGradio:
         
         # Initialize transforms
         self.transform = transforms.Compose([
-            transforms.Resize((256, 256)),
+            transforms.Resize((512, 512)),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
